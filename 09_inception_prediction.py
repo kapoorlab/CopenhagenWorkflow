c3365fd5ac62b36be380788498495097dc264fa8
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-08-31 11:13:45 +0000

    ready for sixth dataset run

--- 09_inception_prediction.py
+++ 09_inception_prediction.py
@@ -25,7 +25,7 @@ data_frames_dir = os.path.join(tracking_directory, f'dataframes/')
 master_xml_name = 'master_' + 'marching_cubes_filled_' + channel + timelapse_to_track + ".xml"
 xml_path = Path(os.path.join(tracking_directory, master_xml_name))
 model_dir = f'{home_folder}Mari_Models/TrackModels/'
-
+ 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
