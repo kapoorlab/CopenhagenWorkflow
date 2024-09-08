a9a60fba96a9c9331fdbf0cd3c50c863018b2e46
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-09-08 19:19:49 +0000

    model

--- 01_membrane_segmentation_cellpose.py
+++ 01_membrane_segmentation_cellpose.py
@@ -16,29 +16,23 @@ configstore.store(name='VollCellSegPose', node=VollCellSegPose)
 def main(config: VollCellSegPose):
     
     image_dir = config.experiment_data_paths.dual_channel_split_directory
-    den_model_dir = config.model_paths.den_model_dir
-    edge_enhancement_model_name = config.model_paths.edge_enhancement_model_name
-    save_dir = os.path.join(image_dir, 'MembraneSeg')
+    save_dir = os.path.join(image_dir, 'VollCellPoseSeg')
     Path(save_dir).mkdir(exist_ok=True)
     
     cellpose_model_name = config.model_paths.cellpose2D_model_name
     
     diameter_cellpose = config.parameters.diameter_cellpose
     stitch_threshold = config.parameters.stitch_threshold
-    channel_membrane = config.parameters.channel_membrane
     cellpose_model_dir = config.model_paths.cellpose2D_model_dir
     flow_threshold = config.parameters.flow_threshold
     cellprob_threshold = config.parameters.cellprob_threshold
     gpu = config.parameters.gpu
-    n_tiles = config.parameters.n_tiles
 
     Raw_path = os.path.join(image_dir, config.parameters.file_type)
     filesRaw = glob.glob(Raw_path)
     filesRaw =natsorted(filesRaw)
-    channel_membrane = config.parameters.channel_membrane
     axes = config.parameters.axes
     do_3D = config.parameters.do_3D
-    edge_enhancement_model = CARE(config = None, name = edge_enhancement_model_name, basedir = den_model_dir)
     for fname in filesRaw:
                     
                     result_file = os.path.join(save_dir, 'CellPose', f'{os.path.splitext(os.path.basename(fname))[0]}.tif')
@@ -46,8 +40,6 @@ def main(config: VollCellSegPose):
                         print(f"Skipping {fname} as {result_file} already exists.")
                         continue 
 
-                    image = imread(fname)
-                    image_membrane = image[:, channel_membrane, :, :]
                     Name = os.path.basename(os.path.splitext(fname)[0])
                     extension = os.path.splitext(fname)[1]
                     inner_folder_path = os.path.join(save_dir, 'CellPose')  
@@ -56,8 +48,8 @@ def main(config: VollCellSegPose):
                     if not os.path.exists(os.path.join(inner_folder_path, Name + extension)):
                             
 
-                            denoised_image_membrane = VollSeg3D(image_membrane,unet_model = None, star_model = None,  noise_model=edge_enhancement_model,n_tiles= n_tiles, dounet=False,  axes='ZYX')
-                            imwrite(edge_enhanced_folder_path + '/' + Name + '.tif', denoised_image_membrane)  
+                            denoised_image_membrane = imread(os.path.join(edge_enhanced_folder_path, Name + extension))
+
                             CellPoseSeg( denoised_image_membrane, 
                             diameter_cellpose= diameter_cellpose,
                             stitch_threshold = stitch_threshold,
