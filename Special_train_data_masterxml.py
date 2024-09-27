964280f63a8969315ae411c7b459088d8f2780db
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-09-27 17:08:20 +0000

    base dataset name

--- Special_train_data_masterxml.py
+++ Special_train_data_masterxml.py
@@ -29,12 +29,12 @@ base_dataset_name = 'Fifth'
 cell_type = 'Goblet'
 home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 timelapse_to_track = f'timelapse_{base_dataset_name.lower()}_dataset'
-tracking_directory = f'{home_folder}Mari_Data_Oneat/Mari_{dataset_name}_Dataset_Analysis/nuclei_membrane_tracking/'
+tracking_directory = f'{home_folder}Mari_Data_Oneat/Mari_{dataset_name}/nuclei_membrane_tracking/'
 channel = 'nuclei_'
 
 master_xml_name = 'master_' + 'marching_cubes_filled_' + channel + timelapse_to_track + ".xml"
 xml_path = Path(os.path.join(tracking_directory, master_xml_name))
-oneat_detections = f'{home_folder}Mari_Data_Oneat/Mari_{dataset_name}_Dataset_Analysis/oneat_detections/non_maximal_oneat_mitosis_locations_{channel}timelapse_{dataset_name.lower()}_dataset.csv'
+oneat_detections = f'{home_folder}Mari_Data_Oneat/Mari_{dataset_name}/oneat_detections/non_maximal_oneat_mitosis_locations_{channel}timelapse_{dataset_name.lower()}_dataset.csv'
 
 
 save_dir = os.path.join(tracking_directory, f'distribution_plots/')
