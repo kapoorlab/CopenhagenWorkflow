1c97193a47243b4f2e29dba1668a8629188c2434
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-09-30 12:09:41 +0000

    pre reelease

--- 08_cell_type_msd.py
+++ 08_cell_type_msd.py
@@ -220,8 +220,9 @@ for index, cell_type_index in enumerate(cell_type):
         cell_type_label = key_list[index]
         for cluster_plot in cluster_plots:
                     filtered_tracks = cluster_extended_shape_dataframe[cluster_extended_shape_dataframe["Shape_Cluster_Label_Type"] == cell_type_index]
-                    filtered_tracks = filtered_tracks.dropna(subset=['t', cluster_plot, hue_option])
-                    for hue_option in hue_options:                 
+                    
+                    for hue_option in hue_options:  
+                        filtered_tracks = filtered_tracks.dropna(subset=['t', cluster_plot, hue_option])               
                         plt.figure(figsize=(15, 6))
                         print(cluster_plot, len(filtered_tracks['t']), len(filtered_tracks[cluster_plot]), len(filtered_tracks[hue_option]))
                         scatter = plt.scatter(filtered_tracks['t'],filtered_tracks[cluster_plot],c=filtered_tracks[hue_option],cmap='viridis')
