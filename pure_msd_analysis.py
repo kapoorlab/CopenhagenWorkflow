a2afafbc9b3accb95c59fee33d5edc1a6e8b2d61
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-09-30 14:21:28 +0000

    cell type

--- pure_msd_analysis.py
+++ pure_msd_analysis.py
@@ -22,7 +22,7 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
 # %%
-dataset_name = 'Second
+dataset_name = 'Second'
 home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 timelapse_to_track = f'timelapse_{dataset_name.lower()}_dataset'
 tracking_directory = f'{home_folder}Mari_Data_Oneat/Mari_{dataset_name}_Dataset_Analysis/nuclei_membrane_tracking/'
