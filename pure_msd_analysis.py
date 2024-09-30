886083228ae284b6d89f2ad15ad2210e5ec718d8
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-09-30 14:23:41 +0000

    cell type

--- pure_msd_analysis.py
+++ pure_msd_analysis.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import os
 import numpy as np
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import norm
