521d4afd4d19013c223ab36501d9bdc21e266e6a
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-10-10 11:31:12 +0000

    densenet with attention

--- train_gbr_neural_net_dynamic.py
+++ train_gbr_neural_net_dynamic.py
@@ -16,13 +16,16 @@ home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 channel = 'nuclei_'
 base_dir =  f'{home_folder}Mari_Data_Training/track_training_data/'
 model_dir = f'{home_folder}Mari_Models/TrackModels/'
-dynamic_model_dir = os.path.join(model_dir, f'dynamic_feature_lightning_densenet_gbr_25_{channel}lite/')
+dynamic_model_dir = os.path.join(model_dir, f'dynamic_feature_lightning_attention_gbr_25_{channel}shallowest_litest/')
 os.makedirs(dynamic_model_dir, exist_ok = True)
 dynamic_gbr_h5_file = f'dynamic_training_data_gbr_25_{channel}.h5'
 num_classes = 3
 batch_size = 9800
 epochs = 100
-block_config = (6,12,24)
+block_config = (6)
+attention_dim=64
+cutoffs=(25,)
+n_pos=(8,)
 growth_rate = 4
 train_gbr_neural_net(
     save_path = dynamic_model_dir,
@@ -30,10 +33,13 @@ train_gbr_neural_net(
     num_classes = num_classes,
     batch_size = batch_size,
     epochs = epochs,
-    model_type = 'densenet',
-    experiment_name='dynamic_densenet',
+    model_type = 'attention',
+    experiment_name='dynamic_attention',
     num_workers = 10,
     block_config=block_config,
+    attention_dim = attention_dim,
+    cutoffs = cutoffs,
+    n_pos = n_pos,
     growth_rate = growth_rate
 )
 
