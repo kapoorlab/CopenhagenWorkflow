fb3312acd301874dca797caebd9f5c3bea0e8058
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-10-15 14:11:22 +0000

    scripts for multiple jobs

--- train_gbr_neural_net_morpho.py
+++ train_gbr_neural_net_morpho.py
@@ -1,46 +1,43 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 import os
-from napatrackmater.Trackvector import (
-    train_gbr_neural_net
-)
+import argparse
+from napatrackmater.Trackvector import train_gbr_neural_net
 
+# Argument parsing to configure parameters for multiple runs
+parser = argparse.ArgumentParser(description='Train Morphodynamic Neural Network with Attention.')
+parser.add_argument('--morpho_model_dir', type=str, required=True, help='Path to save the trained model.')
+parser.add_argument('--block_config', type=str, required=True, help='Configuration of blocks (e.g., "(6, 12, 24)").')
+parser.add_argument('--growth_rate', type=int, default=32, help='Growth rate for the model.')
+parser.add_argument('--channel', type=str, default='nuclei_', help='Channel type (e.g., "membrane_", "nuclei_").')
+parser.add_argument('--morpho_gbr_h5_file', type=str, required=True, help='H5 file containing training data.')
 
+args = parser.parse_args()
 
+# Convert block_config from string to tuple of integers
 
+block_config_str = args.block_config.strip("()")
+if "," in block_config_str:
+    block_config = tuple(map(int, block_config_str.split(',')))
+else:
+    block_config = (int(block_config_str),)
+# Static configurations
 home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
-#/home/debian/jz/
-#/gpfsstore/rech/jsy/uzj81mi/
-channel = 'nuclei_'
-base_dir =  f'{home_folder}Mari_Data_Training/track_training_data/'
-model_dir = f'{home_folder}Mari_Models/TrackModels/'
-morpho_model_dir = os.path.join(model_dir, f'morpho_feature_lightning_attention_gbr_25_{channel}')
-os.makedirs(morpho_model_dir, exist_ok = True)
-morpho_gbr_h5_file = f'morphodynamic_training_data_gbr_25_{channel}.h5'
-num_classes = 3
-batch_size = 98000//2
-epochs = 100
-block_config = (6,12,24)
-attention_dim=64
-n_pos=(8,)
-growth_rate = 32
+base_dir = f'{home_folder}Mari_Data_Training/track_training_data/'
+
+# Create model save directory
+os.makedirs(args.morpho_model_dir, exist_ok=True)
+
+# Train the model
 train_gbr_neural_net(
-    save_path = morpho_model_dir,
-    h5_file = os.path.join(base_dir, morpho_gbr_h5_file),
-    num_classes = num_classes,
-    batch_size = batch_size,
-    epochs = epochs,
-    model_type = 'attention',
+    save_path=args.morpho_model_dir,
+    h5_file=os.path.join(base_dir, args.morpho_gbr_h5_file),
+    num_classes=3,
+    batch_size=98000 // 2,
+    epochs=100,
+    model_type='attention',
     experiment_name='morpho_attention',
-    num_workers = 10,
+    num_workers=10,
     block_config=block_config,
-    attention_dim = attention_dim,
-    n_pos = n_pos,
-    growth_rate = growth_rate
+    attention_dim=64,
+    n_pos=(8,),
+    growth_rate=args.growth_rate,
 )
-
-
-
-
--- train_gbr_neural_net_shape.py
+++ train_gbr_neural_net_shape.py
@@ -1,46 +1,48 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 
 import os
-from napatrackmater.Trackvector import (
-    train_gbr_neural_net
-)
+import argparse
+from napatrackmater.Trackvector import train_gbr_neural_net
 
+# Argument parsing
+parser = argparse.ArgumentParser(description='Train Neural Network with Attention.')
+parser.add_argument('--shape_model_dir', type=str, required=True, help='Path to save the trained model.')
+parser.add_argument('--block_config', type=str, required=True, help='Configuration of blocks (e.g., "(6, 12)").')
+parser.add_argument('--growth_rate', type=int, default=4, help='Growth rate for the model.')
+parser.add_argument('--channel', type=str, default='membrane_', help='Channel type (e.g., "membrane_", "nuclei_").')
+parser.add_argument('--shape_gbr_h5_file', type=str, required=True, help='H5 file containing training data.')
 
+args = parser.parse_args()
 
+# Convert block_config from string to tuple of integers
 
+block_config_str = args.block_config.strip("()")
+if "," in block_config_str:
+    block_config = tuple(map(int, block_config_str.split(',')))
+else:
+    block_config = (int(block_config_str),)
+# Static configurations
 home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
-#/gpfsstore/rech/jsy/uzj81mi/
-channel = 'membrane_'
-base_dir =  f'{home_folder}Mari_Data_Training/track_training_data/'
-model_dir = f'{home_folder}Mari_Models/TrackModels/'
-shape_model_dir = os.path.join(model_dir, f'shape_feature_lightning_attention_gbr_25_{channel}shallowest_litest/')
-os.makedirs(shape_model_dir, exist_ok = True)
-shape_gbr_h5_file = f'shape_training_data_gbr_25_{channel}.h5'
-num_classes = 3
-batch_size = 102400
-epochs = 100
-block_config = (6)
-attention_dim=64
-n_pos=(8,)
-growth_rate = 4
+base_dir = f'{home_folder}Mari_Data_Training/track_training_data/'
+
+# Create model save directory
+os.makedirs(args.shape_model_dir, exist_ok=True)
+
+# Train the model
 train_gbr_neural_net(
-    save_path = shape_model_dir,
-    h5_file = os.path.join(base_dir, shape_gbr_h5_file),
-    num_classes = num_classes,
-    batch_size = batch_size,
-    epochs = epochs,
+    save_path=args.shape_model_dir,
+    h5_file=os.path.join(base_dir, args.shape_gbr_h5_file),
+    num_classes=3,
+    batch_size=102400,
+    epochs=100,
     model_type='attention',
     experiment_name='shape_attention',
-    num_workers = 10,
-    block_config = block_config,
-    attention_dim = attention_dim,
-    n_pos = n_pos,
-    growth_rate = growth_rate,
-    
+    num_workers=10,
+    block_config=block_config,
+    attention_dim=64,
+    n_pos=(8,),
+    growth_rate=args.growth_rate,
 )
+
 
 
 
