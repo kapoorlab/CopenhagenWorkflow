67075cfe2197a760d10eada7952a7c358a0213cf
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-10-16 15:56:58 +0000

    make data

---
+++ slurm_scripts/jobscript_train_gbr_morpho_second.py
@@ -0,0 +1,61 @@
+import subprocess
+
+# Define arrays for the different parameter values for morpho models
+morpho_model_dirs_base = [
+    "morpho_feature_attention_shallower_litest",
+    "morpho_feature_attention_shallower_liter",
+    "morpho_feature_attention_shallower_lite",
+    "morpho_feature_attention_shallower"
+]
+
+block_configs = [
+    "(6,12)",
+    "(6,12)",
+    "(6,12)",
+    "(6,12)"
+]
+
+growth_rates = [4, 8, 16, 32]
+
+channels = [
+    "nuclei_",
+    "membrane_"
+]
+
+morpho_gbr_h5_files = [
+    "morphodynamic_training_data_gbr_25_nuclei_.h5",
+    "morphodynamic_training_data_gbr_25_membrane_.h5"
+]
+
+# Loop through each model configuration
+for i, morpho_model_base in enumerate(morpho_model_dirs_base):
+    block_config = block_configs[i]
+    growth_rate = growth_rates[i]
+
+    # Loop through each channel and submit a separate job
+    for j, channel in enumerate(channels):
+        morpho_gbr_h5_file = morpho_gbr_h5_files[j]
+        
+        # Include the channel name in the model directory
+        morpho_model_dir = f"/lustre/fsn1/projects/rech/jsy/uzj81mi/Mari_Models/TrackModels/{morpho_model_base}_{channel}/"
+        
+        # Construct the sbatch command
+        sbatch_command = [
+            "sbatch",
+            "--nodes=1",
+            "-A", "jsy@a100",
+            "-C", "a100",
+            "--gres=gpu:1",
+            "--partition=gpu_p5",
+            "--cpus-per-task=40",
+            "--time=20:00:00",
+            f"--job-name=Morpho_{i}_{j}",
+            f"--output=morpho_{i}_{j}.o%j",
+            f"--error=morpho_{i}_{j}.e%j",
+            "--wrap",
+            f"module purge && module load anaconda-py3 && conda deactivate && conda activate capedenv && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/lib/ && export XLA_FLAGS=--xla_gpu_cuda_data_dir=/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/ && module load cuda/11.8.0 && python /gpfswork/rech/jsy/uzj81mi/CopenhagenWorkflow/train_gbr_neural_net_morpho.py --morpho_model_dir {morpho_model_dir} --block_config {block_config} --growth_rate {growth_rate} --channel {channel} --morpho_gbr_h5_file {morpho_gbr_h5_file}"
+        ]
+        
+        # Submit the job
+        subprocess.run(sbatch_command)
+        
