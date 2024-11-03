374c40d64e0687bd2aefe1f260cdd0e9ac0f8bf9
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-11-03 18:45:31 +0000

    ready to train vision

--- train_data_gen_oneat_inception.py
+++ train_data_gen_oneat_inception.py
@@ -1,11 +1,12 @@
 import os
 from pathlib import Path
-from napatrackmater import create_h5, filter_and_get_tracklets
+from napatrackmater import create_h5, filter_and_get_tracklets, normalize_image_in_chunks
 import pandas as pd
-
+import numpy as np
+from tifffile import imwrite, imread
 def process_datasets(home_folder, dataset_names, image_dataset_names, image_folder_names,  channel, train_save_dir, tracking_directory_name='nuclei_membrane_tracking/', time_window = 10, crop_size = [256,256,8]):
-
-
+ 
+    dtype=np.float32
     for idx, dataset_name in enumerate(dataset_names):
         image_dataset_name = image_dataset_names[idx]
         image_folder_name = image_folder_names[idx]
@@ -15,7 +16,11 @@ def process_datasets(home_folder, dataset_names, image_dataset_names, image_fold
         data_frames_dir = os.path.join(tracking_directory, 'dataframes/')
         normalized_dataframe_file = os.path.join(data_frames_dir, f'goblet_basal_dataframe_normalized_{channel}.csv')
         dataset_dataframe = pd.read_csv(normalized_dataframe_file)
-        
+        if isinstance(raw_image, str):
+            raw_image = imread(raw_image)
+        if isinstance(segmentation_image, str):
+                segmentation_image = imread(segmentation_image)    
+                raw_image = normalize_image_in_chunks(raw_image.astype(dtype))
         cell_type_dataframe = dataset_dataframe[~dataset_dataframe['Cell_Type'].isna()]
         class_map_gbr = {
             0: "Basal",
@@ -28,19 +33,19 @@ def process_datasets(home_folder, dataset_names, image_dataset_names, image_fold
             filter_and_get_tracklets(cell_type_dataframe, cell_type, time_window, raw_image, crop_size, segmentation_image, dataset_name, train_save_dir, 
                                 train_label)
        
-        create_h5(train_save_dir,train_size=0.95,save_name="cellfate_vision_training_data_gbr")
+    create_h5(train_save_dir,train_size=0.95,save_name="cellfate_vision_training_data_gbr")
  
     
 
 home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 dataset_name = [
-    'Second_Dataset_Analysis', 'Fifth_Dataset_Analysis', 'Sixth_Dataset_Analysis', 
+     'Fifth_Dataset_Analysis', 'Sixth_Dataset_Analysis', 
     'Fifth_Extra_Goblet', 'Fifth_Extra_Radial', 'Third_Extra_Goblet', 'Third_Extra_Radial']
 image_folder_name = [
-    'Second_Dataset_Analysis', 'Fifth_Dataset_Analysis', 'Sixth_Dataset_Analysis', 
+    'Fifth_Dataset_Analysis', 'Sixth_Dataset_Analysis', 
     'Fifth_Dataset_Analysis', 'Fifth_Dataset_Analysis', 'Third_Dataset_Analysis', 'Third_Dataset_Analysis']
 image_dataset_name = [
-    'Second', 'Fifth', 'Sixth', 
+     'Fifth', 'Sixth', 
     'Fifth', 'Fifth', 'Third', 'Third']
 time_window = 25
 crop_size = [128,128,8]
