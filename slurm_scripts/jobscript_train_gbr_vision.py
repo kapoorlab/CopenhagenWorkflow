5388538aebb9b1f4e7222c87805aa8e6e653fb04
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-11-08 20:58:41 +0000

    train vision

--- slurm_scripts/jobscript_train_gbr_vision.py
+++ slurm_scripts/jobscript_train_gbr_vision.py
@@ -13,7 +13,7 @@ vision_gbr_h5_files = [
 for i, morpho_model_base in enumerate(gbr_vision_model_dirs):
         input_shape = [10,128,128,8]
 
-        vision_gbr_h5_file = vision_gbr_h5_files[j]
+        vision_gbr_h5_file = vision_gbr_h5_files[i]
         
         morpho_model_dir = f"/lustre/fsn1/projects/rech/jsy/uzj81mi/Mari_Models/TrackModels/vision_track_training_data/"
         
@@ -28,9 +28,9 @@ for i, morpho_model_base in enumerate(gbr_vision_model_dirs):
             #"--partition=gpu_p2",
             "--cpus-per-task=40",
             "--time=20:00:00",
-            f"--job-name=Morpho_{i}_{j}",
-            f"--output=morpho_{i}_{j}.o%j",
-            f"--error=morpho_{i}_{j}.e%j",
+            f"--job-name=Morpho_{i}",
+            f"--output=morpho_{i}.o%j",
+            f"--error=morpho_{i}.e%j",
             "--wrap",
             f"module purge && module load anaconda-py3 && conda deactivate && conda activate capedenv && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/lib/ && export XLA_FLAGS=--xla_gpu_cuda_data_dir=/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/ && module load cuda/11.8.0 && python /gpfswork/rech/jsy/uzj81mi/CopenhagenWorkflow/train_gbr_neural_net_vision.py --vision_model_dir {morpho_model_dir} --input_shape {input_shape} --batch_size {batch_size} --vision_gbr_h5_file {vision_gbr_h5_file}"
         ]
