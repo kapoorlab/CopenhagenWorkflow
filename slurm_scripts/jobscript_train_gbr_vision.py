f876eb41a169322616b5643de913530346070cb4
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-11-08 21:41:09 +0000

    dataload

--- slurm_scripts/jobscript_train_gbr_vision.py
+++ slurm_scripts/jobscript_train_gbr_vision.py
@@ -1,22 +1,6 @@
 import subprocess
 
-gbr_vision_model_dirs = [
-    "vision_track_training_data"
-]
-
-
-vision_gbr_h5_files = [
-    "cellfate_vision_training_data_gbr.h5",
-]
-
-for i, morpho_model_base in enumerate(gbr_vision_model_dirs):
-        
-
-        vision_gbr_h5_file = vision_gbr_h5_files[i]
-        
-        morpho_model_dir = f"/lustre/fsn1/projects/rech/jsy/uzj81mi/Mari_Models/TrackModels/vision_track_training_data/"
-        
-        sbatch_command = [
+sbatch_command = [
            "sbatch",
             "--nodes=1",
             "-A", "jsy@a100",
@@ -31,8 +15,8 @@ for i, morpho_model_base in enumerate(gbr_vision_model_dirs):
             f"--output=morpho_{i}.o%j",
             f"--error=morpho_{i}.e%j",
             "--wrap",
-            f"module purge && module load anaconda-py3 && conda deactivate && conda activate capedenv && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/lib/ && export XLA_FLAGS=--xla_gpu_cuda_data_dir=/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/ && module load cuda/11.8.0 && python /gpfswork/rech/jsy/uzj81mi/CopenhagenWorkflow/train_gbr_neural_net_vision.py --vision_model_dir {morpho_model_dir} --vision_gbr_h5_file {vision_gbr_h5_file}"
+            f"module purge && module load anaconda-py3 && conda deactivate && conda activate capedenv && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/lib/ && export XLA_FLAGS=--xla_gpu_cuda_data_dir=/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/ && module load cuda/11.8.0 && python /gpfswork/rech/jsy/uzj81mi/CopenhagenWorkflow/train_gbr_neural_net_vision.py"
         ]
         
-        subprocess.run(sbatch_command)
+subprocess.run(sbatch_command)
         
--- train_gbr_neural_net_vision.py
+++ train_gbr_neural_net_vision.py
@@ -1,15 +1,11 @@
 import os
-import argparse
+from pathlib import Path
 from napatrackmater.Trackvector import train_gbr_vision_neural_net
 
-# Argument parsing to configure parameters for multiple runs
-parser = argparse.ArgumentParser(description='Train Cell Fate Vision Model.')
-parser.add_argument('--vision_model_dir', type=str, required=True, help='Path to save the trained model.')
-parser.add_argument('--batch_size', type=int, default=64, help='Batch Size.')
-parser.add_argument('--vision_gbr_h5_file', type=str, required=True, help='H5 file containing training data.')
 
+vision_model_dir = '/lustre/fsn1/projects/rech/jsy/uzj81mi/Mari_Models/TrackModels/vision_inception/'
 
-args = parser.parse_args()
+Path(vision_model_dir).mkdir(exist_ok=True)
 depth = {'depth_0': 6,'depth_1': 12,'depth_2': 24,'depth_3': 16 }
 stage_number = 4
 input_shape = [10,128,128,8]
@@ -17,12 +13,12 @@ batch_size = 32
 
 home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 base_dir = f'{home_folder}Mari_Data_Training/vision_track_training_data/'
-
+vision_gbr_h5_file = 'cellfate_vision_training_data_gbr.h5'
 
 
 train_gbr_vision_neural_net(
-    save_path=args.vision_model_dir,
-    h5_file=os.path.join(base_dir, args.vision_gbr_h5_file),
+    save_path=vision_model_dir,
+    h5_file=os.path.join(base_dir, vision_gbr_h5_file),
     input_shape = input_shape,
     num_classes=3,
     batch_size=batch_size,
