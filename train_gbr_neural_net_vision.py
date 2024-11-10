8388a0f572eb6871535cd6ab6bbff03853d6ef6f
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-11-10 11:06:39 +0000

    cpu only

--- train_gbr_neural_net_vision.py
+++ train_gbr_neural_net_vision.py
@@ -10,6 +10,7 @@ depth = {'depth_1': 12,'depth_2': 24,'depth_3': 16 }
 input_shape = [25,8,64,64]
 batch_size = 16
 crop_size = [8, 64, 64]
+growth_rate = 1
 home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 base_dir = f'{home_folder}Mari_Data_Training/vision_track_training_data/'
 vision_gbr_h5_file = 'cellfate_vision_training_data_gbr.h5'
@@ -25,5 +26,6 @@ train_gbr_vision_neural_net(
     depth=depth,
     experiment_name='vision_cellfate',
     num_workers=10,
-    crop_size = crop_size
+    crop_size = crop_size,
+    growth_rate = growth_rate
 )
