36dd2bb3487869a9f4482edd58d54f6558ad6e21
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-11-15 18:18:27 +0000

    line

--- slurm_scripts/jobscript_train_gbr_vision.py
+++ slurm_scripts/jobscript_train_gbr_vision.py
@@ -11,9 +11,9 @@ sbatch_command = [
             #"--partition=gpu_p2",
             "--cpus-per-task=40",
             "--time=20:00:00",
-            f"--job-name=Morpho_{i}",
-            f"--output=morpho_{i}.o%j",
-            f"--error=morpho_{i}.e%j",
+            f"--job-name=Vision",
+            f"--output=vision.o%j",
+            f"--error=vision.e%j",
             "--wrap",
             f"module purge && module load anaconda-py3 && conda deactivate && conda activate capedenv && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/lib/ && export XLA_FLAGS=--xla_gpu_cuda_data_dir=/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/ && module load cuda/11.8.0 && python /gpfswork/rech/jsy/uzj81mi/CopenhagenWorkflow/train_gbr_neural_net_vision.py"
         ]
