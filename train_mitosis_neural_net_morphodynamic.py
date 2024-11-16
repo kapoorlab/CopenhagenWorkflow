9b4899fe2896b6770aa97132251000b14455903e
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-11-16 19:08:52 +0000

    morphos

--- train_mitosis_neural_net_morphodynamic.py
+++ train_mitosis_neural_net_morphodynamic.py
@@ -11,7 +11,7 @@ home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 base_dir =  f'{home_folder}Mari_Data_Training/track_training_data/'
 model_dir = f'{home_folder}Mari_Models/TrackModels/'
 
-dynamic_model_dir = os.path.join(model_dir, 'morphodynamic_features_mitosis')
+dynamic_model_dir = os.path.join(model_dir, 'morphodynamic_features_mitosis_gr16')
 os.makedirs(dynamic_model_dir, exist_ok = True)
 morphodynamic_mitosis_h5_file = 'morphodynamic_training_data_mitosis_nuclei_25.h5'
 
@@ -19,7 +19,7 @@ num_classes = 2
 batch_size = 98000
 epochs = 100
 block_config = (6)
-growth_rate = 4
+growth_rate = 16
 
 train_mitosis_neural_net(
     h5_file = os.path.join(base_dir, morphodynamic_mitosis_h5_file),
