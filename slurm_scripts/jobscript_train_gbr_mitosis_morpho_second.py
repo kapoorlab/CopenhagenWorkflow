3614aae8951bc10cc2c70bff6024f3fb822a0421
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-11-17 18:05:19 +0000

    transforms

---
+++ slurm_scripts/jobscript_train_gbr_mitosis_morpho_second.py
@@ -0,0 +1,51 @@
+import subprocess
+
+
+block_configs = [
+    "6",
+    "6",
+   
+]
+
+growth_rates = [8, 16, 32]
+
+channels = [
+    "nuclei_",
+   
+]
+
+morpho_gbr_h5_files = [
+    "morphodynamic_training_data_mitosis_nuclei_50.h5",
+   
+]
+
+for i in enumerate(growth_rates):
+    block_config = block_configs[i]
+    growth_rate = growth_rates[i]
+
+    for j, channel in enumerate(channels):
+        morpho_gbr_h5_file = morpho_gbr_h5_files[j]
+        
+        morpho_model_dir = f"/lustre/fsn1/projects/rech/jsy/uzj81mi/Mari_Models/TrackModels/morphodynamic_feature_mitosis_50_growth_rate_{growth_rate}/"
+        
+        sbatch_command = [
+            "sbatch",
+            "--nodes=1",
+            "-A", "jsy@a100",
+            "-C", "a100",
+            #"-A", "jsy@v100",
+            "--gres=gpu:1",
+            "--partition=gpu_p5",
+            #"--partition=gpu_p2",
+            "--cpus-per-task=40",
+            "--time=20:00:00",
+            f"--job-name=Morpho_{i}_{j}",
+            f"--output=morpho_{i}_{j}.o%j",
+            f"--error=morpho_{i}_{j}.e%j",
+            "--wrap",
+            f"module purge && module load anaconda-py3 && conda deactivate && conda activate capedenv && export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/lib/ && export XLA_FLAGS=--xla_gpu_cuda_data_dir=/linkhome/rech/gengoq01/uzj81mi/.conda/envs/capedenv/ && module load cuda/11.8.0 && python /gpfswork/rech/jsy/uzj81mi/CopenhagenWorkflow/train_mitosis_neural_net_morphodynamic.py --morpho_model_dir {morpho_model_dir} --block_config {block_config} --growth_rate {growth_rate}  --morpho_gbr_h5_file {morpho_gbr_h5_file}"
+        ]
+        
+        # Submit the job
+        subprocess.run(sbatch_command)
+        
