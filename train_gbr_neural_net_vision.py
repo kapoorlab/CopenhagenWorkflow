d67570b8e4be1862b49fd87b63e9b9f81fa76d98
kapoorlab <varun.kapoor@kapoorlabs.org>
2024-11-18 20:03:29 +0000

    augs

--- train_gbr_neural_net_vision.py
+++ train_gbr_neural_net_vision.py
@@ -3,14 +3,14 @@ from pathlib import Path
 from napatrackmater.Trackvector import train_gbr_vision_neural_net
 
 
-vision_model_dir = '/lustre/fsn1/projects/rech/jsy/uzj81mi/Mari_Models/TrackModels/vision_inception/'
+vision_model_dir = '/lustre/fsn1/projects/rech/jsy/uzj81mi/Mari_Models/TrackModels/vision_inception_growth_rate_32/'
 
 Path(vision_model_dir).mkdir(exist_ok=True)
 depth = {'depth_0': 12,'depth_1': 24,'depth_2': 16 }
 crop_size = [75, 8, 128, 128]
 input_shape = crop_size
 batch_size = 16
-growth_rate = 4
+growth_rate = 32
 home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 base_dir = f'{home_folder}Mari_Data_Training/vision_track_training_data_large_window_75/'
 vision_gbr_h5_file = 'cellfate_vision_training_data_gbr_large.h5'
