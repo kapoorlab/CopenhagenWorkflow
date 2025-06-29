f51d20032c7bed1fbab0bb741365fd05f2914133
kapoorlab <varun.kapoor@kapoorlabs.org>
2025-06-29 14:56:51 +0000

    add qkv model

--- train_gbr_neural_net_morpho.py
+++ train_gbr_neural_net_morpho.py
@@ -24,8 +24,8 @@ from napatrackmater.Trackvector import train_gbr_neural_net
 
 home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 base_dir = f'{home_folder}Mari_Data_Training/track_training_data/'
-morpho_model_base = "membrane_inception_cell_type"
-channel = 'membrane_'
+morpho_model_base = "nuclei_inception_qkv_cell_type"
+channel = 'nuclei_'
 morpho_model_dir = f"/lustre/fsn1/projects/rech/jsy/uzj81mi/Mari_Models/TrackModels/{morpho_model_base}/"
 morpho_gbr_h5_file= f"inception_cell_type_{channel}.h5"
 
@@ -34,6 +34,8 @@ os.makedirs(morpho_model_dir, exist_ok=True)
 block_config = (6)
 growth_rate = 4
 augment = False
+attn_heads = 8
+seq_len = 25
 
 train_gbr_neural_net(
     save_path=morpho_model_dir,
@@ -41,12 +43,14 @@ train_gbr_neural_net(
     num_classes=3,
     batch_size=98000,
     epochs=100,
-    model_type='attention',
+    model_type='qkv',
     experiment_name='morpho_attention',
     num_workers=10,
     block_config=block_config,
     attention_dim=64,
     n_pos=(8,),
     growth_rate=growth_rate,
-    augment = augment
+    augment = augment,
+    attn_heads = attn_heads,
+    seq_len = seq_len
 )
--- train_mitosis_neural_net_morphodynamic.py
+++ train_mitosis_neural_net_morphodynamic.py
@@ -11,7 +11,7 @@ home_folder = '/lustre/fsn1/projects/rech/jsy/uzj81mi/'
 base_dir =  f'{home_folder}Mari_Data_Training/track_training_data/'
 model_dir = f'{home_folder}Mari_Models/TrackModels/'
 
-dynamic_model_dir = os.path.join(model_dir, 'inception_motion_type_nuclei_')
+dynamic_model_dir = os.path.join(model_dir, 'inception_qkv_motion_type_nuclei_')
 os.makedirs(dynamic_model_dir, exist_ok = True)
 morphodynamic_mitosis_h5_file = 'inception_motion_type_nuclei_.h5'
 
@@ -20,20 +20,23 @@ batch_size = 98000
 epochs = 100
 block_config = (6)
 growth_rate = 16
-
+attn_heads = 8
+seq_len = 25
 train_mitosis_neural_net(
     h5_file = os.path.join(base_dir, morphodynamic_mitosis_h5_file),
     save_path = dynamic_model_dir,
     num_classes = num_classes,
     batch_size = batch_size,
     epochs = epochs,
-    model_type = 'attention',
+    model_type = 'qkv',
     experiment_name='morphodynamic_mitosis_densenet',
     num_workers = 10,
     block_config=block_config,
     growth_rate = growth_rate,
     attention_dim=64,
-    n_pos=(8,)
+    n_pos=(8,),
+    attn_heads = attn_heads,
+    seq_len = seq_len
 )
 
 
